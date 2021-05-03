0c29014748ff7cc2087d826ce17f0026dfeb8845
nao <nao_h13@hotmail.com>
2021-05-04 07:21:41 +0900

    CSSフレームワークの Warter.css を使うように変更

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css'
+  })
+  
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
